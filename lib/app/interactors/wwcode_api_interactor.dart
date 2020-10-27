042e7e43504bdae52151f2cde934fec4fb80d25a
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-10-27 18:02:10 -0400

    added network call to wwcode_api_interactor

--- lib/app/interactors/wwcode_api_interactor.dart
+++ lib/app/interactors/wwcode_api_interactor.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:async/async.dart';
 import 'package:flutter_app/app/clients/wwcode_api_client.dart';
 import 'package:flutter_app/data/models/member.dart';
+import 'package:flutter_app/data/models/quote.dart';
 import 'package:flutter_app/utils/app_exception.dart';
 import 'package:http/http.dart';
 
@@ -19,6 +20,17 @@ class WWCodeApiInteractor {
     return _runNetworkAction(networkAction.then((response) {
       final List<dynamic> json = jsonDecode(response.body);
       return json.map((e) => Member.fromJson(e)).toList();
+    }));
+  }
+
+  Future<Result<List<Quote>>> getAllQuotes() async {
+    final Future<Response> networkAction = _apiClient.get(
+      _apiClient.url('/wwcode-cincy-database/quotes'),
+    );
+
+    return _runNetworkAction(networkAction.then((response) {
+      final List<dynamic> json = jsonDecode(response.body);
+      return json.map((e) => Quote.fromJson(e)).toList();
     }));
   }
 
